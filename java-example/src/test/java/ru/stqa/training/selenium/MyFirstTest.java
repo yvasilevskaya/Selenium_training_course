21f31c67052d72cc2e9cc85e4d7f7368b0b6c5bd
Yulia <yulya.vasilevskaya@gmail.com>
2017-07-12 15:42:29 +0300

    Added fullscreen option

--- java-example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -5,7 +5,12 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
@@ -20,7 +25,9 @@ public class MyFirstTest{
 
   @Before
   public void start(){
-    driver = new ChromeDriver();
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("start-fullscreen");
+    driver = new ChromeDriver(options);
     wait = new WebDriverWait(driver,10);
   }
 
